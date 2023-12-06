5729b97b464e79e74dae122a8ce48ddc52d4c0b6
TillW <14997061+x789@users.noreply.github.com>
2023-12-06 15:59:41 +0100

    Add solution of day 4, part 1

---
+++ Day04/PuzzleSolver.cs
@@ -0,0 +1,19 @@
+﻿
+// Copyright (c) 2023 - TillW
+// Licensed to you under the MIT License
+internal sealed class PuzzleSolver
+{
+	internal int CalculateTotalPoints(IEnumerable<string> input)
+	{
+		var points = 0;
+		foreach (var card in input)
+		{
+			var (left, right, _) = card.Split("|");
+			var winningNumbers = left.Split(":").Last().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => uint.Parse(x));
+			var myNumbers = right.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => uint.Parse(x));
+			var countOfWinningNumbers = winningNumbers.Intersect(myNumbers).Count();
+			points += (int)Math.Pow(2, countOfWinningNumbers - 1);
+		}
+		return points;
+	}
+}
