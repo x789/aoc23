0d6df1051ec1947685f5e2b40392ce26dc12d9e0
TillW <14997061+x789@users.noreply.github.com>
2023-12-06 17:44:12 +0100

    Add solution of day 4, part 2

--- Day04/PuzzleSolver.cs
+++ Day04/PuzzleSolver.cs
@@ -5,15 +5,35 @@ internal sealed class PuzzleSolver
 {
 	internal int CalculateTotalPoints(IEnumerable<string> input)
 	{
-		var points = 0;
+		return GetMatchingNumbers(input).Sum(x => (int)Math.Pow(2, x-1));
+	}
+
+	internal int CalculateNumberOfScratchCards(IEnumerable<string> input)
+	{
+		var numberOfCards = 0;
+		var cards = GetMatchingNumbers(input).Select(x => (Count: 1, MatchingNumbers: x)).ToArray();
+		for (int i = 0; i < cards.Length; i++)
+		{
+			var card = cards[i];
+			numberOfCards += card.Count;
+			if (card.MatchingNumbers > 0)
+				for (int j = i+1; j < i + card.MatchingNumbers + 1; j++)
+					cards[j].Count += cards[i].Count;
+		}
+		return numberOfCards;
+	}
+
+	private int[] GetMatchingNumbers(IEnumerable<string> input)
+	{
+		var matchingNumbers = new List<int>();
 		foreach (var card in input)
 		{
 			var (left, right, _) = card.Split("|");
 			var winningNumbers = left.Split(":").Last().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => uint.Parse(x));
 			var myNumbers = right.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => uint.Parse(x));
-			var countOfWinningNumbers = winningNumbers.Intersect(myNumbers).Count();
-			points += (int)Math.Pow(2, countOfWinningNumbers - 1);
+			var matchingNumbersOnCard = winningNumbers.Intersect(myNumbers).Count();
+			matchingNumbers.Add(matchingNumbersOnCard);
 		}
-		return points;
+		return matchingNumbers.ToArray();
 	}
 }
